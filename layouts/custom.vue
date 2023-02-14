1a4d944f10d77f1217bef27429ef89a10abcb8bf
William Krisley <will.ks2101@gmail.com>
2023-02-13 21:19:21 -0300

    custom layout for logged users

---
+++ layouts/custom.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="custom">
+    <SideBar :sideBar="openedSideBar" @toggleSide="toggleSideBar" />
+    <main class="content">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      openedSideBar: false,
+    };
+  },
+  methods: {
+    toggleSideBar() {
+      console.log("ok");
+      this.openedSideBar = !this.openedSideBar;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.custom {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+.content {
+  width: 100%;
+  height: 100vh;
+  padding-left: 35px;
+}
+</style>
