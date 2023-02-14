f5749dddd2c100f402912d0770b676515ff7eab2
William Krisley <will.ks2101@gmail.com>
2023-02-13 21:18:34 -0300

    pre config for middleware to secure routes

---
+++ middleware/auth.ts
@@ -0,0 +1,10 @@
+const isAuthenticated = () => {
+  return false;
+};
+
+export default defineNuxtRouteMiddleware((to, from) => {
+  // isAuthenticated() is an example method verifying if a user is authenticated
+  if (isAuthenticated() === false) {
+    return navigateTo("/login");
+  }
+});
