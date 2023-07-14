9b875019ceee416ac3bceb6a1db0798f4713cfe6
william krisley <will.ks2101@gmail.com>
2023-07-14 10:32:59 -0300

    update the show date

--- components/SalesListMain.vue
+++ components/SalesListMain.vue
@@ -1,31 +1,35 @@
 <template>
-    <div class="item">
-        <h3 class="desc">
-            {{ data.title }}
-        </h3>
-        <h3 class="desc">{{ formatToPrice(((data.total - data.discount_for_client) / 100)) }}</h3>
-        <h3 class="desc">{{ formatData(data.updated_at) }}</h3>
-    </div>
+  <div class="item">
+    <h3 class="desc">
+      {{ data.title }}
+    </h3>
+    <h3 class="desc">
+      {{ formatToPrice((data.total - data.discount_for_client) / 100) }}
+    </h3>
+    <h3 class="desc">{{ formatData(data.sold_at) }}</h3>
+  </div>
 </template>
 
 <script>
+import { formatToPrice } from "~/composables/formatationFunctions";
+
 export default {
-    props: {
-        data: Object
-    }
-}
+  props: {
+    data: Object,
+  },
+};
 </script>
 
 <style scoped>
 .item {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .desc {
-    font-size: 1rem;
-    font-weight: 300;
+  font-size: 1rem;
+  font-weight: 300;
 }
 </style>
