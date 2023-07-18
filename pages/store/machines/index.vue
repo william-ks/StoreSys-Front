612507da2f90fefae6dc66d17663a839ef2fd5e2
william krisley <will.ks2101@gmail.com>
2023-07-18 14:30:22 -0300

    new clean page for machines

---
+++ pages/store/machines/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="content">
+      <div class="center"></div>
+    </div>
+  </template>
+  
+  <script setup>
+  useSeoMeta({
+    title: "Maquininhas",
+  });
+  </script>
+    
+  <script>
+  definePageMeta({
+    middleware: ["auth"],
+  });
+  
+  export default {};
+  </script>
+    
+  <style scoped>
+  </style>
