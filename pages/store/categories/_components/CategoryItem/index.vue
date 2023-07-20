18823689878e703b90dcd0abc6e2b31c93f1782b
william krisley <will.ks2101@gmail.com>
2023-07-20 02:16:05 -0300

    dividido trabalho de componentes

---
+++ pages/store/categories/_components/CategoryItem/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="categoriesItem">
+    <span>{{ data.description }}</span>
+    <Icon name="humbleicons:dots-vertical" />
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object,
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
