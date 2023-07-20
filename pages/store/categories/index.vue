e2dfec851acad2250bb592be20a6e75312af050e
william krisley <will.ks2101@gmail.com>
2023-07-20 00:41:01 -0300

    base para a pagina de categorias

--- pages/store/categories/index.vue
+++ pages/store/categories/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="content">
-    <div class="center"></div>
+    <div class="center">
+      <button class="button add">
+        <Icon name="material-symbols:add" />
+        <span>Adicionar categoria</span>
+      </button>
+      <ul class="categoriesList">
+        <li
+          class="categoriesItem"
+          v-for="category of categoriesList"
+          :key="category.id"
+        >
+          <span>{{ category.description }}</span>
+          <Icon @click="handleAddCategory" name="humbleicons:dots-vertical" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
+import categoriesFunctions from "~/composables/contextFunctions/categoriesFunctions";
+
 useSeoMeta({
   title: "Categorias",
 });
@@ -15,8 +32,24 @@ definePageMeta({
   middleware: ["auth"],
 });
 
-export default {};
+export default {
+  data() {
+    return {
+      categoriesList: [],
+    };
+  },
+  methods: {
+    handleAddCategory() {
+      console.log("here");
+    },
+  },
+  async mounted() {
+    const { content } = await categoriesFunctions.download();
+    this.categoriesList = content;
+  },
+};
 </script>
   
 <style scoped>
+@import url("./style.css");
 </style>
